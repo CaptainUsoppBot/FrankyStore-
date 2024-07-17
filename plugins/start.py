93658b7878a4b469c604a1e2afd5b2f93e423178
CaptainUsoppBot <inanstcompany@gmail.com>
2024-07-17 12:40:46 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -89,7 +89,7 @@ async def start_command(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton("⚡️ ᴀʙᴏᴜᴛ", callback_data = "about"),
-                    InlineKeyboardButton('🍁 sᴇʀɪᴇsғʟɪx', url='https://t.me/Team_Netflix/40')
+                    InlineKeyboardButton('🍁 Rᴇᴘᴏʀᴛ Aᴅᴍɪɴ ', url='https://t.me/abidabdullah199')
                 ]
             ]
         )
